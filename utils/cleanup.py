dcdac64bc0e9da161ca4ca6947c2e0df937ab76d
omxpro <omsenjalia@gmail.com>
2022-07-24 11:52:32 +0000

    New Videos

--- utils/cleanup.py
+++ utils/cleanup.py
@@ -1,27 +1,3 @@
-import os
-from os.path import exists
-
-
-def cleanup() -> int:
-    """Deletes all temporary assets in assets/temp
-
-    Returns:
-        int: How many files were deleted
-    """
-    if exists("./assets/temp"):
-        count = 0
-        files = [f for f in os.listdir(".") if f.endswith(".mp4") and "temp" in f.lower()]
-        count += len(files)
-        for f in files:
-            os.remove(f)
-        try:
-            for file in os.listdir("./assets/temp/mp4"):
-                count += 1
-                os.remove("./assets/temp/mp4/" + file)
-        except FileNotFoundError:
-            pass
-        for file in os.listdir("./assets/temp/mp3"):
-            count += 1
-            os.remove("./assets/temp/mp3/" + file)
-        return count
-    return 0
+version https://git-lfs.github.com/spec/v1
+oid sha256:98a6fa2f8296f8fff2b7b2182b1cad7ffd743c92b6ff6992431922fccddc04ae
+size 757
