ec373aa70d6fcc4f29e764d2db3b3bdc13dd493a
Edge <69719569+omxpro@users.noreply.github.com>
2022-07-24 11:28:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -76,13 +76,20 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         print_markdown("## Clearing temp files")
         cleanup()
-res = command.run(['git add .'])
-res1 = command.run(['git commit -m "internal commit"']) 
-res2 = command.run(['git push'])
+res = command.run(['git config --global user.name "omxpro"'])
+res1 = command.run(['git config --global user.email "omsenjalia@gmail.com"'])
+res2 = command.run(['git add .'])
+res3 = command.run(['git commit -m "internal commit"']) 
+res4 = command.run(['git push'])
 
 print(res.output) 
 print(res1.output) 
 print(res2.output) 
+print(res3.output) 
+print(res4.output)
+ 
 print(res.exit)
 print(res1.exit)
 print(res2.exit)
+print(res3.exit)
+print(res4.exit)
